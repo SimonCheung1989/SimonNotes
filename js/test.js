dd3e840a07f23a711e9efc72eed7f6d2b3af4968
243120877 <243120877@qq.com>
2017-03-18 14:44:33 +0800

    smartgit

---
+++ js/test.js
@@ -0,0 +1,15 @@
+let i = 10;
+console.log(i);
+
+let [a, b, c] = [3, 4, 5];
+
+console.log(a);
+
+console.log(b);
+
+console.log(c);
+
+let j = () => 10;
+console.log(j());
+
+console.log("Hello World!");
